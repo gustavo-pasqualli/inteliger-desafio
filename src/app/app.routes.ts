e1eb4102ae66116cc27bf930bc975bdf674ba1fe
Switch1511 <gustavopasqualli0@gmail.com>
2025-09-21 18:40:00 -0300

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home').then(c => c.Home),
+  },
+  {
+    path: 'perfil',
+    loadComponent: () =>
+      import('./features/profile/profile').then(c => c.Profile),
+  },
+
+  { path: '**', redirectTo: 'home' },
+];
